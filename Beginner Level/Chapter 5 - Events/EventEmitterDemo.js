f6488671473b2d3d42bcd9562114e35384a75861
Saloni Jain <saloni.juit@gmail.com>
2014-11-16 23:16:30 +0530

    Updated the presentation and code
    Saloni Jain

--- Beginner Level/Chapter 5 - Events/EventEmitterDemo.js
+++ Beginner Level/Chapter 5 - Events/EventEmitterDemo.js
@@ -4,7 +4,7 @@ function Door(colour){
 	this.colour = colour;
 	events.EventEmitter.call(this); // executes the constructor of the event emitter
 
-	this.open = function(){
+	this.open = function(){ //how many times the door the opened, this will emit an event
 		this.emit('open'); // emitting an event of type open
 	};
 }
@@ -12,6 +12,8 @@ function Door(colour){
 Door.prototype._proto_ = events.EventEmitter.prototype; //copies all of the event emitter properties to the door object
 
 var frontdoor = new door('blue');
+
+//listener
 frontdoor.on('open',function(){
 		console.log('ring ring');
 });

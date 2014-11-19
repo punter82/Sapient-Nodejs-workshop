9e95fa5bc53830282ebe09f06024cab586137ff8
Prasanna Venkatesh <punter.ss@gmail.com>
2014-11-19 20:11:34 +0530

    Working with Files

---
+++ Advanced Level/Working with Files/file.js
@@ -0,0 +1,62 @@
+var fs = require('fs');
+
+//Write a file
+/*fs.writeFile('results.txt', 'Hello World', function(err) {
+  if(err) throw err;
+  console.log('File write completed');
+});*/
+
+
+// Read a file
+/*fs.readFile('results.txt', 'utf8', function(err, data) {
+  // the data is passed to the callback in the second argument
+  console.log(data);
+});*/
+
+
+/*fs.unlink('sample/test.jpg', function (err) {
+  if (err) throw err;
+  console.log('successfully deleted the file');
+});*/
+
+
+/*fs.rename('sample/IMG_20141022_184619.jpg', 'sample/photo.jpg', function (err) {
+  if (err) throw err;
+  console.log('renamed complete');
+});*/
+
+
+/*var path = 'sample/';
+fs.readdir(path, function (err, files) {
+  if(err) throw err;
+  files.forEach(function(file) {
+    console.log(path+file);
+    fs.stat(path+file, function(err, stats) {
+      //console.log(stats);
+    });
+  });
+});*/
+
+/*fs.mkdir('newdir', 0777, function(err) {
+  if(err) throw err;
+  console.log('Created newdir');
+});*/
+
+/*
+fs.rmdir('./newdir', function(err) {
+    if(err) throw err;
+    console.log('Removed newdir');
+});
+*/
+
+/*fs.writeFile('newdir/test.json', '{"key": "value"}', function(error){
+  if(err) throw err;
+  console.log('File write completed');
+});*/
+
+fs.readFile('newdir/test.json', 'utf8', function(err, data) {
+  if(err) throw err;
+  console.log(data);
+});
+
+
